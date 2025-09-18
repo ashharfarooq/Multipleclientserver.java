0425853cd921d74823bc24a2183767ad1a969cbe
Ashhar Farooq <ashharfarooq736@gmail.com>
2025-09-18 00:38:58 -0700

    Update Deskserver.java

--- Deskserver.java
+++ Deskserver.java
@@ -1,51 +1,54 @@
 import java.util.*;
-import java.io.*;
 import java.net.*;
+import java.io.*;
+	class Threading extends Thread{
+		private Socket client;
+		public Threading (Socket client){
+			this.client=client;
+		}
+		public void run(){
+			try{
+			Scanner in = new Scanner(client.getInputStream());
+		PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+		Scanner input = new Scanner(System.in);
+	
+		while(true){
 
-class Threading extends Thread {
-    private Socket client;
-
-    public Threading(Socket client) {
-        this.client = client;
-    }
-
-    public void run() {
-        try {
-            Scanner in = new Scanner(client.getInputStream());
-            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-
-            while (true) {
-                String line = in.nextLine();
-                System.out.println(line);
-                if (line.equals("exit")) {
-                    break;
-                }
-            }
-
-            client.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	
+	String message = in.nextLine();
+	System.out.println("Client says "+ message);
+	
+    
+	if(message.equals("exit")){
+		break;
 }
+String line = input.nextLine();
+	out.println(line);
+}
+client.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	}
 
-public class Deskserver {
-    public static void main(String[] args) {
-        try {
-            ServerSocket server = new ServerSocket(9090); // waiting for client connection
-            System.out.println("Waiting for client connection");
-while(true){
-            Socket client = server.accept();
-            System.out.println("Client Connected");
 
-            Threading t = new Threading(client);
-            t.start();
 
-}       
+public class Deskserver{
+	public static void main(String[] args){
+try{
+		ServerSocket server = new ServerSocket(9090);
+	System.out.println("Waiting for client connection");
+	while(true){
+		Socket client = server.accept();
+	System.out.println("client connected");
+		Threading handle = new Threading(client);
+		handle.start();
+	}
 
-     
- } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+}catch(IOException e){
+e.printStackTrace();
+}
+}
 }
